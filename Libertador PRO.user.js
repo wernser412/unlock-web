b17a34cad93e5b461a6b8f09c09f7de585b4abe7
wernser412 <53667212+wernser412@users.noreply.github.com>
2025-12-08 19:13:11 -0500

    Update and rename Libertador de la Web.user.js to Libertador PRO.user.js

--- Libertador PRO.user.js
+++ Libertador PRO.user.js
@@ -4,6 +4,8 @@
 // @version      2025.12.08
 // @description  Menús claros, selección de texto, tooltip que cambia de color y copia URL con CTRL sobre imagen, desbloqueo total de atajos
 // @author       wernser412
+// @icon         https://github.com/wernser412/unlock-web/raw/refs/heads/main/ICONO.svg
+// @downloadURL  https://github.com/wernser412/unlock-web/raw/refs/heads/main/Libertador%20de%20la%20Web.user.js
 // @match        *://*/*
 // @run-at       document-start
 // @grant        GM_setValue
